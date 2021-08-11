a910070d71a7f97d1a1defb83c08bafc8abccce8
kimkj1108 <kimkj1108@gmail.com>
2021-08-11 12:14:19 +0900

    자바복습_연산자

---
+++ MySelfJava/src/com/my/calculator/CalEx01.java
@@ -0,0 +1,21 @@
+package com.my.calculator;
+
+import java.util.Scanner;
+
+public class CalEx01 {
+	public static void main(String[] args) {
+//		System.out.println("값을 입력하세요");
+//		Scanner sc = new Scanner(System.in);
+//		int i = sc.nextInt();
+//		System.out.println("3으로 나눈 나머지 값"+i%3);
+		
+		int i;
+		int j;
+		i=10;
+		j=30;
+		System.out.println(i>j);//false
+		System.out.println(i>j ? "참":"거짓");//삼항연산자 제일 햇갈린다.
+		
+	}
+
+}
