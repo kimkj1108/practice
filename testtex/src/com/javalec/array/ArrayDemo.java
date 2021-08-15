aa6ce54e274486bb2fe8359ff008ec24d79f9f62
kimkj1108 <kimkj1108@gmail.com>
2021-08-15 21:39:06 +0900

    기본 복습중

---
+++ testtex/src/com/javalec/array/ArrayDemo.java
@@ -0,0 +1,15 @@
+package com.javalec.array;
+
+public class ArrayDemo {
+	public static void main(String[] args) {
+		int[] arr = {1,2,3,4};
+		System.out.println(arr[2]);
+		
+		int[] arr2 = new int[3];//배열길이가 3인 arr배열 선언, 길이가 3이기에 인덱스는 2까지 있을 것이다.
+		arr2[0] = 1;
+		arr2[1] = 2;
+		arr2[2] = 2;
+		
+	}
+
+}
