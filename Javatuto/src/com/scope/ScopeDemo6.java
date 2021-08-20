73d2fdcbe5a7a6f2fbcdbfe3a4723517fa200f71
kimkj1108 <kimkj1108@gmail.com>
2021-08-20 22:01:12 +0900

    scope2

--- Javatuto/src/com/scope/ScopeDemo6.java
+++ Javatuto/src/com/scope/ScopeDemo6.java
@@ -1,5 +1,16 @@
 package com.scope;
 
 public class ScopeDemo6 {
-
+	static int i = 5;
+	
+	static void a() {
+		int i = 10;
+		b();
+	}
+	static void b() {
+		System.out.println(i);
+	}
+	public static void main(String[] args) {
+		a();
+	}
 }
