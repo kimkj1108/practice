b8a61f8c7fab7658419954b666f87bf3d3b60636
kimkj1108 <kimkj1108@gmail.com>
2021-08-22 22:22:15 +0900

    jar

---
+++ Practice/src/com/crud/test01/Test01.java
@@ -0,0 +1,11 @@
+package com.crud.test01;
+
+public class Test01 {
+	public static void main(String[] args) throws ClassNotFoundException {
+		
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		
+		
+	}
+
+}
