f9b8b53124faf2f28abc0fc0e12f5321152c2de7
kimkj1108 <kimkj1108@gmail.com>
2021-08-23 22:28:14 +0900

    db연결 연습

--- Practice/src/com/crud/test01/Test01.java
+++ Practice/src/com/crud/test01/Test01.java
@@ -21,14 +21,27 @@ public class Test01 {
 		Connection con = DriverManager.getConnection(url, user, password);
 					
 		//3. query 준비
-		String sql = "";
+		String sql = " SELECT EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO  "
+				   + " FROM EMP ";//앞뒤 ""안에 문장안에 공백 무조건 넣어야 한다.쿼리문 안에 세미콜론 쓰면 안됨.
 		Statement stmt = con.createStatement();
 		//4. query 리턴 및 실행
 		ResultSet rs = stmt.executeQuery(sql);
 		//Result 객체 = 
 		while(rs.next()) {
-			System.out.printf();
+			System.out.printf("%2d %10s %10s %6d %10s %7.2f %7.2f %3d \n",
+					rs.getInt("EMPNO"),//위에 작성한 컬럼 이름이나
+					rs.getString(2),	//위에 작성한 컬럼 번호를 적는다.
+					rs.getString("JOB"),
+					rs.getInt(4),
+					rs.getDate("HIREDATE"),
+					rs.getDouble(6),
+					rs.getDouble("COMM"),
+					rs.getInt(8));
 		}
+		//5. db종료
+		rs.close();
+		stmt.close();
+		con.close();
 		
 		
 		
