718893a8c82394b58490c46e4917d8c594f27a9c
kimkj1108 <kimkj1108@gmail.com>
2021-08-25 20:43:56 +0900

    Constructor

---
+++ Javatuto/src/com/constructor/calculatorDemo1.java
@@ -0,0 +1,35 @@
+package com.constructor;
+
+
+//Calculator클래스가 선안되었다.
+class Calculator{
+	int left, right;
+	//Calculator클래스 안에 이름이 동일한 Calculator메소드가 선언되었다.
+	//이것을 생성자(Constructor)라고 한다.
+	//생성자는
+	public Calculator(int left, int right) {
+		this.left = left;
+		this.right = right;
+	}
+	public void sum() {
+		System.out.println(this.left+this.right);
+	}
+	public void avg() {
+		System.out.println((this.left+this.right)/2);	
+	}
+}
+	
+
+public class calculatorDemo1 {
+	public static void main(String[] args) {
+		Calculator c1 = new Calculator(10, 20);
+		c1.sum();
+		c1.avg();
+		
+		Calculator c2 = new Calculator(20, 40);
+		c2.sum();
+		c2.avg();
+				
+	}
+
+}
