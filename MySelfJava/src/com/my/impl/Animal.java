994185b04b397b29c0ff6a1bfca63c6f5258deff
kimkj1108 <kimkj1108@gmail.com>
2021-08-31 20:48:48 +0900

    interface

---
+++ MySelfJava/src/com/my/impl/Animal.java
@@ -0,0 +1,9 @@
+package com.my.impl;
+
+public interface Animal {
+	//인터페이스에 abstract 키워드는 붙여도 별 변화가 없다.
+	
+	public abstract void eat();
+	void move();
+
+}
---
+++ MySelfJava/src/com/my/impl/Bird.java
@@ -0,0 +1,15 @@
+package com.my.impl;
+
+public class Bird implements Animal {
+
+	@Override
+	public void eat() {
+		
+	}
+	
+	@Override
+	public void move() {
+		
+	}
+
+}
