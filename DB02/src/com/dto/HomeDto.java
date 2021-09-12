d7157a7497c70fa0293b71a748cc06295e2ace47
kimkj1108 <kimkj1108@gmail.com>
2021-09-12 20:19:57 +0900

    create dto

--- DB02/src/com/dto/HomeDto.java
+++ DB02/src/com/dto/HomeDto.java
@@ -1,5 +1,23 @@
 package com.dto;
 
+//값 전달 객체
+//Data Transfer Object(= Value Object)
 public class HomeDto {
+	
+	private int h_num;
+	private String h_name;
+	private String h_job;
 
+	
+	/*
+	//기본생성자
+	public HomeDto(int update_h_num, String update_h_name, String update_h_job) {
+	}
+	//파라미터 초기화 하는 생성자
+	public HomeDto(int h_num, String h_name, String h_job) {
+		super();
+		
+	}
+	*/
+	
 }
