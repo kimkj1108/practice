07f36072006ea9b5579acfd1886f8be889886830
kimkj1108 <kimkj1108@gmail.com>
2021-09-18 20:43:20 +0900

    Controller

--- DB02/src/com/controller/HomeController.java
+++ DB02/src/com/controller/HomeController.java
@@ -1,8 +1,14 @@
 package com.controller;
 
+import java.util.Scanner;
+
 //화면 제어
 public class HomeController {
 	
+	private static Scanner sc = new Scanner(System.in);
 	
-
+	public static int getMenue() {
+		StringBuffer sb = new StringBuffer();
+		return 0;
+	}
 }
