324209e77a7f8f4ec55cfaead5e47e60dad31a3d
kimkj1108 <kimkj1108@gmail.com>
2021-09-19 21:24:40 +0900

    controller

--- DB02/src/com/controller/HomeController.java
+++ DB02/src/com/controller/HomeController.java
@@ -9,6 +9,9 @@ public class HomeController {
 	
 	public static int getMenue() {
 		StringBuffer sb = new StringBuffer();
+		sb.append("****\n").append("1.전체출력\n").append("2.선택출력\n")
+		.append("3.추   가\n").append("4.수   정\n").append("5.삭   제\n").append("6.종   료\n").append("input select\n");
+		
 		return 0;
 	}
 }
