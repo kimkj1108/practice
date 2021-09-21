d7d318ba7e392fa5602986d7b16e6e0801b10602
kimkj1108 <kimkj1108@gmail.com>
2021-09-21 22:00:44 +0900

    controller main bing...

--- DB02/src/com/controller/HomeController.java
+++ DB02/src/com/controller/HomeController.java
@@ -1,7 +1,9 @@
 package com.controller;
 
+import java.util.List;
 import java.util.Scanner;
 
+import com.biz.HomeBiz;
 import com.dto.HomeDto;
 
 //화면 제어
@@ -9,7 +11,7 @@ public class HomeController {
 	
 	private static Scanner sc = new Scanner(System.in);
 	
-	public static int getMenue() {
+	public static int getMenu() {
 		StringBuffer sb = new StringBuffer();
 		sb.append("****\n").append("1.전체출력\n").append("2.선택출력\n")
 		.append("3.추   가\n").append("4.수   정\n").append("5.삭   제\n").append("6.종   료\n").append("input select\n");
@@ -31,6 +33,22 @@ public class HomeController {
 		return 0;
 	}
 	public static void main(String[] args) {
+		HomeBiz biz = new HomeBiz();
+		
+		int select = 0;
+		
+		while(select != 6) {
+			select = getMenu();
+			switch (select) {
+			case 1:
+				System.out.println("전체 출력");
+				List<HomeDto> list = biz.selectList();
+				for (HomeDto dto : list) {
+					System.out.println(dto);
+					break;
+				}
+			}
+		}
 		
 	}
 	
