49f6d54249b654d0de0442da44db6165418b7156
kimkj1108 <kimkj1108@gmail.com>
2021-09-22 12:15:12 +0900

    insert case3 in controller compleat

--- DB02/src/com/controller/HomeController.java
+++ DB02/src/com/controller/HomeController.java
@@ -8,36 +8,40 @@ import com.dto.HomeDto;
 
 //화면 제어
 public class HomeController {
-	
+
 	private static Scanner sc = new Scanner(System.in);
-	
+
 	public static int getMenu() {
 		StringBuffer sb = new StringBuffer();
-		sb.append("****\n").append("1.전체출력\n").append("2.선택출력\n")
-		.append("3.추   가\n").append("4.수   정\n").append("5.삭   제\n").append("6.종   료\n").append("input select\n");
-		System.out.println(sb);//sb에 각 메뉴를 주르륵 출력하게 하였고 이것을 출력하였다
-		int select  = sc.nextInt();//int 타입 select라는 변수에 스케너를 통해 입력받은 값을 대입.
+		sb.append("****\n").append("1.전체출력\n").append("2.선택출력\n").append("3.추   가\n").append("4.수   정\n")
+				.append("5.삭   제\n").append("6.종   료\n").append("input select\n");
+		System.out.println(sb);// sb에 각 메뉴를 주르륵 출력하게 하였고 이것을 출력하였다
+		int select = sc.nextInt();// int 타입 select라는 변수에 스케너를 통해 입력받은 값을 대입.
 		return select;
 	}
-	//선택출력
+
+	// 선택출력
 	public HomeDto selectOne(int h_num) {
-		
+
 		return null;
 	}
-	//추가
+
+	// 추가
 	public int insert(HomeDto dto) {
 		return 0;
 	}
-	//삭제
+
+	// 삭제
 	public int delete(int h_num) {
 		return 0;
 	}
+
 	public static void main(String[] args) {
 		HomeBiz biz = new HomeBiz();
-		
+
 		int select = 0;
-		
-		while(select != 6) {
+
+		while (select != 6) {
 			select = getMenu();
 			switch (select) {
 			case 1:
@@ -54,11 +58,26 @@ public class HomeController {
 				HomeDto selectOneDto = biz.selectOne(h_num);
 				System.out.println(selectOneDto);
 				break;
+			case 3:
+				System.out.println("추가");
+				// 이름,직업
+				System.out.println("이름");
+				String h_name = sc.next();
+				System.out.println("직업");
+				String h_job = sc.next();
 				
+				HomeDto insertDto = new HomeDto(0, h_name, h_job);
+				int insertRes = biz.insert(insertDto);
+				if(insertRes > 0) {
+					System.out.println("추가 성공!");
+				}else {
+					System.out.println("추가 실패!");
+				}
+				break;
+			
 			}
 		}
-		
+
 	}
-	
-	
+
 }
